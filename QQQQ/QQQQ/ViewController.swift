73f6b90c1a9ae1b2454a0da5fc1da27911db6480
zhanfucai <996279724@qq.com>
2016-07-17 14:58:00 +0800

    dfgd

---
+++ QQQQ/QQQQ/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QQQQ
+//
+//  Created by zfc on 16/7/17.
+//  Copyright © 2016年 zfc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
